464b54ecf5795ffae4dfab33a70f62085dc8ed8b
jgeofil <jeremy@thehyve.nl>
2015-09-10 23:50:49 -0400

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name studiojeimsonApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the studiojeimsonApp
+ */
+angular.module('studiojeimsonApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name studiojeimsonApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the studiojeimsonApp
+ */
+angular.module('studiojeimsonApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
