326fe21892888337e124db8c7fb75cda6b85c730
Jeisson Drenth <Jeimson@Jeissons-MacBook-Pro.local>
2015-10-12 11:07:26 -0400

    specializes

---
+++ app/scripts/controllers/calendar (1).js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name studiojeimsonApp.controller:ClientsCtrl
+ * @description
+ * # ClientsCtrl
+ * Controller of the studiojeimsonApp
+ */
+angular.module('studiojeimsonApp')
+  .controller('CalendarCtrl', function () {
+  });
